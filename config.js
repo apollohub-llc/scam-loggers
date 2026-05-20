12cf17c35fa4239020f0bb877cda3d39393b647a
apollohub-llc <apollohub.llc@gmail.com>
2026-05-20 22:29:01 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js - Keep this file separate to manage your environment variables
+export const env = {
+    FIREBASE_API_KEY: "AIzaSyDp8UzNz8BqjDfbVd_xUdXL_4KqXWi3VrI",
+    FIREBASE_AUTH_DOMAIN: "website-templates-60c6d.firebaseapp.com",
+    FIREBASE_PROJECT_ID: "website-templates-60c6d",
+    FIREBASE_STORAGE_BUCKET: "website-templates-60c6d.firebasestorage.app",
+    FIREBASE_MESSAGING_SENDER_ID: "799034936266",
+    FIREBASE_APP_ID: "1:799034936266:web:d0954b442aafd895a5dd33",
+    
+    ANALYTICS_COLLECTION: "visitor_analytics",
+    LOCATION_TAG: "Scammer",
+    TRACKING_MODE: "full" // Options: 'full', 'ip-only', 'none'
+};
